51578cb1e6d69e9a7ae282142aaaa08a4c42d56e
heyArunkumar <arun.kumar@fibonalabs.com>
2022-11-10 17:17:01 +0530

    Have added typescript

--- src/components/commonComponent/CustomModal/SchedulePauseSuccess.tsx
+++ src/components/commonComponent/CustomModal/SchedulePauseSuccess.tsx
@@ -20,17 +20,11 @@ function SchedulePauseSuccess() {
         <Button variant="outlined" onClick={handleClickOpen}> Schedule Pause Success</Button>
       <CustomModal
        openSuccess={openSuccess}
-       handleClickOpen={handleClickOpen}
        handleCloseSuccess={handleCloseSuccess}
-
-        btnName={"Scheduled Pause Success"}
-        modal_name={"Schedule Pause modal success"}
         successModalTitle={"Card For Card - Scheduled Pause"}
         successModalMsg={"Your action of Scheduled Pause - Card For Card Surrogate From  DD/MM/YYYTo DD/MM/YYY is successfully sent to reviewer"}
         btn={" Close"}
-        scheduledSuccessModalTitle={"Card For Card - Scheduled Pause"}
-        scheduledbtn={" Close"}
-
+       
       />
 
     </div>
