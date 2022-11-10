af3c9f75346b3d7eba2589b7314729768075a2c0
heyArunkumar <arun.kumar@fibonalabs.com>
2022-11-10 13:39:43 +0530

    added datepicker

--- src/components/commonComponent/CustomModal/CustomModal.tsx
+++ src/components/commonComponent/CustomModal/CustomModal.tsx
@@ -9,7 +9,7 @@ import TextareaAutosize from "@mui/material/TextareaAutosize";
 import TextField from "@mui/material/TextField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/lab";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
@@ -21,11 +21,14 @@ import { colors } from "../../../style/Color";
 import card_catalogue_sucess_icon from "../../../assets/icons/card_catalogue_sucess_icon.svg";
 import card_catalogue_rejecte_icon from "../../../assets/icons/modal_rejected_icon.svg";
 
+
+
 function CustomModal(props: any) {
   // const classess = useStyles();
 
   const [pauseStatus, setPauseStatus] = useState(props.normalPause);
-  const [value, setValue] = useState(null);
+  const [startDatevalue, setStartDateValue] = useState(null);
+  const [endDatevalue, setEndDateValue] = useState(null);
 
   return (
     <Stack className="App">
@@ -74,8 +77,6 @@ function CustomModal(props: any) {
             <DialogContent sx={{ paddingTop: "18px", paddingBottom: "5px" }}>
               <DialogContentText
                 id="alert-dialog-slide-description"
-                // px={4}
-                // py={0}
                 align={"center"}
                 fontSize={16}
                 fontWeight={600}
@@ -95,8 +96,6 @@ function CustomModal(props: any) {
             <DialogContent sx={{ paddingTop: "18px", paddingBottom: "5px" }}>
               <DialogContentText
                 id="alert-dialog-slide-description"
-                // px={4}
-                // py={0}
                 align={"center"}
                 fontSize={16}
                 fontWeight={600}
@@ -117,7 +116,6 @@ function CustomModal(props: any) {
               fontWeight={700}
               align={"center"}
               pb={0}
-              // px={9}
               fontSize={12}
               sx={{
                 padding: {
@@ -135,7 +133,6 @@ function CustomModal(props: any) {
               fontWeight={700}
               align={"center"}
               pb={0}
-              // px={9}
               fontSize={13}
               sx={{
                 padding: {
@@ -227,21 +224,21 @@ function CustomModal(props: any) {
                       className="datePicker_input"
                       toolbarPlaceholder="dd"
                       label={props.datepickerLabelStart}
-                      value={value}
+                      value={startDatevalue}
                       onChange={(newValue: any) => {
-                        setValue(newValue);
+                        setStartDateValue(newValue);
                       }}
                       renderInput={(params: any) => (
-                        <TextField size="small" {...params} />
+                        <TextField size="small" {...params}  />
                       )}
                     />
 
                     <DatePicker
                       className="datePicker_input"
                       label={props.datepickerLabelEnd}
-                      value={value}
+                      value={endDatevalue}
                       onChange={(newValue: any) => {
-                        setValue(newValue);
+                        setEndDateValue(newValue);
                       }}
                       renderInput={(params: any) => (
                         <TextField size="small" {...params} />
@@ -304,10 +301,12 @@ function CustomModal(props: any) {
                 <Grid container>
                   {props.product_label.map((item: any) => {
                     return (
-                      <Grid xs={6} sm={4} key={item.id}>
+                      <Grid item xs={6} sm={4} key={item.id}>
                         {" "}
-                        <FormControlLabel
-                          control={<Checkbox checked={item.defaultChecked==true}/>}
+                        <FormControlLabel 
+                          control={
+                            <Checkbox checked={item.defaultChecked == true} />
+                          }
                           label={item.label}
                         />
                       </Grid>
@@ -323,7 +322,7 @@ function CustomModal(props: any) {
             sx={{
               flexDirection: "row",
               justifyContent: "flex-end",
-              // marginTop: "15px",
+             
             }}
           >
             {props.submit && (
