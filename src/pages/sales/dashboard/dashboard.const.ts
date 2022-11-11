5057ff21a770be149662e85531b616229ee8f4d4
Ridhima Gupta <ridhima.gupta@fibonalabs.com>
2022-11-11 15:25:56 +0530

    added ui for sales dashboard list

---
+++ src/pages/sales/dashboard/dashboard.const.ts
@@ -0,0 +1,58 @@
+export const salesDashboardList: rowsDataInterface[] = [
+    { id: 1, applicationNum: 1234, customerName: 'Jon', mobileNum: 999999999, lead: 'Rajesh', surrogateName: 'Payroll', dateTime: '03May2021,00:00', Policy: ' Pongal_20', channelName:'T.Nagar', processedBy:'Venketsan',kycStatus: "Success(CKYC)", status: "Approved"},
+    { id: 2, applicationNum: 1234, customerName: 'Cersei', mobileNum: 999999999, lead: 'Rajesh', surrogateName: 'Payroll', dateTime: '03May2021,00:00', Policy: ' Pongal_20', channelName:'T.Nagar', processedBy:'Venketsan',kycStatus: "Success(VKYC)", status: "Approved",  },
+    { id: 3, applicationNum: 1234, customerName: 'Jaime', mobileNum: 999999999, lead: 'Rajesh', surrogateName: 'Payroll', dateTime: '03May2021,00:00', Policy: ' Pongal_20', channelName:'T.Nagar', processedBy:'Venketsan',kycStatus: "Progress(CKYC)", status: "In-Progress",  },
+    { id: 4, applicationNum: 1234, customerName: 'Arya', mobileNum: 999999999, lead: 'Rajesh', surrogateName: 'Payroll', dateTime: '03May2021,00:00', Policy: ' Pongal_20', channelName:'T.Nagar', processedBy:'Venketsan',kycStatus: "Success(VKYC)", status: "Approved",  },
+    { id: 5, applicationNum: 1234, customerName: 'Daenerys', mobileNum: 999999999, lead: 'Rajesh', surrogateName: 'Payroll', dateTime: '03May2021,00:00', Policy: ' Pongal_20', channelName:'T.Nagar', processedBy:'Venketsan',kycStatus: "Progress(VKYC)", status: "In-Progress",  },
+    { id: 6, applicationNum: 1234, customerName: 'Jack', mobileNum: 999999999, lead: 'Rajesh', surrogateName: 'Payroll', dateTime: '03May2021,00:00', Policy: ' Pongal_20', channelName:'T.Nagar', processedBy:'Venketsan',kycStatus: "Failure(CKYC)", status: "Rejected", },
+    { id: 7, applicationNum: 1234, customerName: 'Ferrara', mobileNum: 999999999, lead: 'Rajesh', surrogateName: 'Payroll', dateTime: '03May2021,00:00', Policy: ' Pongal_20', channelName:'T.Nagar', processedBy:'Venketsan',kycStatus: "Success(VKYC)", status: "Approved",  },
+    { id: 8, applicationNum: 1234, customerName: 'Rossini', mobileNum: 999999999, lead: 'Rajesh', surrogateName: 'Payroll', dateTime: '03May2021,00:00', Policy: ' Pongal_20', channelName:'T.Nagar', processedBy:'Venketsan',kycStatus: "Progress(VKYC)", status: "Approved",  },
+    { id: 9, applicationNum: 1234, customerName: 'Harvey', mobileNum: 999999999, lead: 'Rajesh', surrogateName: 'Payroll', dateTime: '03May2021,00:00', Policy: ' Pongal_20', channelName:'T.Nagar', processedBy:'Venketsan',kycStatus: "Dropped(KYC)", status: "Dropped",  },
+  ];
+  
+
+  export const statusRowHeading : statusRowHeadingInterface[]= [
+    {header1:'KYC Status',header2:'Status' , header3:'View'},
+  ];
+
+  export const listRowHeading : statusRowHeadingInterface[]= [
+    {header1:'# ',header2:'Application#' , 
+    header3:'Customer Name',
+    header4:' Mobile Number',
+    header5:'Lead',
+    header6:'Surrogate Name',
+    header7:'Date & Time',
+    header8:'Policy',
+    header9:'Channel',
+    header10:'Processed',
+  },
+  ];
+
+export interface rowsDataInterface {
+  id?: number;
+  applicationNum?: number;
+  customerName?: string;
+  mobileNum?: number;
+  lead?: string;
+  surrogateName?: string;
+  dateTime?: string;
+  Policy?: string;
+  channelName?: string;
+  processedBy?: string;
+  kycStatus?: string;
+  status?: string;
+}
+
+export interface statusRowHeadingInterface {
+  header1?: string;
+  header2?: string;
+  header3?: string;
+  header4?: string;
+  header5?: string;
+  header6?: string;
+  header7?: string;
+  header8?: string;
+  header9?: string;
+  header10?: string;
+
+}
